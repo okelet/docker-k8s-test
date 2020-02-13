71216dd0316c8da669eca34bcf055e849e57a3fe
Juan Asensio Sánchez <juan.asensio.sanchez@everis.com>
2020-02-13 17:49:28 +0100

    App route, gunicorn

--- README.md
+++ README.md
@@ -5,13 +5,13 @@
 ```
 cd docker
 docker build -t flasktest .
-docker run --rm -it -p 5000:5000 --name flasktest flasktest
+docker run --rm -it -p 8000:8000 --name flasktest flasktest
 ```
 
-Access to http://127.0.0.1:5000, or curl-it:
+Access to http://127.0.0.1:8000, or curl-it:
 
 ```
-curl 127.0.0.1:5000
+curl 127.0.0.1:8000
 ```
 
 Upload to Docker Hub:
@@ -19,4 +19,22 @@ Upload to Docker Hub:
 ```
 docker tag flasktest okelet/flasktest:latest
 docker push okelet/flasktest:latest
+```
+
+Run from Docker Hub:
+
+```
+docker run --rm -it -p 8000:8000 --name flasktest okelet:flasktest
+```
+
+Set an application/route prefix:
+
+```
+docker run --rm -it -p 8000:8000 --name flasktest -e APP_PREFIX=/myapp okelet:flasktest
+```
+
+So then use:
+
+```
+curl 127.0.0.1:8000/myapp
 ```
--- docker/app.py
+++ docker/app.py
@@ -1,15 +1,42 @@
+from datetime import datetime
 import os
 import socket
 
-from flask import Flask, send_from_directory
+from flask import Flask, Response, send_from_directory
 
+from prefix_middleware import PrefixMiddleware
 
 app = Flask(__name__)
+
+app_prefix = os.environ.get("APP_PREFIX")
+if app_prefix:
+    if not app_prefix.startswith("/"):
+        app_prefix = f"/{app_prefix}"
+    app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix=app_prefix)
+
+index_info = """Hello, World!
+Hostname is {hostname}
+Date/Time is {dt}
+"""
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
-def hello_world():
-    return "Hello, World from {hostname}!\n".format(hostname=socket.gethostname())
+def index():
+    return Response(
+        index_info.format(
+            hostname=socket.gethostname(),
+            dt=datetime.now()
+        ),
+        mimetype='text/plain'
+    )
+
+@app.route('/hello')
+def hello():
+    return "Hello!"
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f"Hello {name}!"
